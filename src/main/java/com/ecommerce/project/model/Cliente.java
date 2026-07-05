977ae94f39a20ae14a76bf697f0137dfb1b884c9
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-07-04 22:30:52 -0300

    feat: adicionar classe Cliente

--- src/main/java/com/ecommerce/project/model/Cliente.java
+++ src/main/java/com/ecommerce/project/model/Cliente.java
@@ -3,6 +3,41 @@ package com.ecommerce.project.model;
 public class Cliente extends Usuario{
     private String cpf;
     private String telefone;
-    //TODO: terminar quando a classe carrinho de compra estiver pronta
-    // sim eu fiz o diagrama de cima para baixo, fui burro
+    private CarrinhoDeCompras carrinho;
+
+    public Cliente() {
+    }
+
+    public Cliente(long id, String nome, String email, String senha, String cpf, String telefone, CarrinhoDeCompras carrinho) {
+        super(id, nome, email, senha);
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.carrinho = carrinho;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public CarrinhoDeCompras getCarrinho() {
+        return carrinho;
+    }
+
+    public void setCarrinho(CarrinhoDeCompras carrinho) {
+        this.carrinho = carrinho;
+    }
+
+
 }
