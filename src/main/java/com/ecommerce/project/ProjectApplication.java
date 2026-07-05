4c90e6bca23c3c15a06fa6e605e8bba833217d20
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-07-05 09:54:24 -0300

    feat: codigo de teste para visualizar o funcionamento do jpa na persistencia de dados

--- src/main/java/com/ecommerce/project/ProjectApplication.java
+++ src/main/java/com/ecommerce/project/ProjectApplication.java
@@ -1,7 +1,12 @@
 package com.ecommerce.project;
 
+import com.ecommerce.project.model.Produto;
+import com.ecommerce.project.repository.ProdutoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class ProjectApplication {
@@ -10,4 +15,16 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ProdutoRepository produtoRepository) {
+		return args -> {
+			Produto p = new Produto();
+			p.setNome("Notebook Gamer");
+			p.setPreco(new BigDecimal("4500.00"));
+			p.setQuantidadeEstoque(10);
+
+			produtoRepository.save(p);
+			System.out.println(">>> Produto salvo com sucesso: " + p.getNome());
+		};
+	}
 }
